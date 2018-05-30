2ee4eabb0d30bdc0b74ec4e4adc0ed23c42f6cd3
Elias Gray <kitwithshorts@gmail.com>
2018-05-30 07:45:48 -0500

    Create lone-sum.js
    Given 3 int values, a b c, return their sum. However, if one of the  values is the same as another of the values, it does not count towards  the sum.

---
+++ lone-sum.js
@@ -0,0 +1,8 @@
+function loneSum(a,b,c){
+sum = a + b + c;
+if (a == b){sum = sum - a - b;}
+if (a == c){sum = sum - a - c;}
+if (b == c){sum = sum - b - c;}
+if(a==b && b==c){sum = 0;}
+return sum;
+}
